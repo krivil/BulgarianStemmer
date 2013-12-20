6260892c86855f67e72992adeedc22a6653e6cc1
Kiril Markov <krivil@gmail.com>
2013-12-20 17:17:07 +0200

    Initial code commit

---
+++ BulgarianStemmer.Console/Program.cs
@@ -0,0 +1,84 @@
+﻿using System.Diagnostics;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace BulgarianStemmer.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QuickTest();
+
+            System.Console.WriteLine();
+            
+            TimeTest();
+
+            System.Console.WriteLine("Press any key to continue . . .");
+            System.Console.ReadKey(true);
+        }
+
+        private static void QuickTest()
+        {
+            var words = new[] { "Управляващите", "ПРОТЕСТИРАЩИТЕ" };
+
+            var stemmer = new BulgarianStemmer();
+
+            foreach (string word in words)
+            {
+                string stem = stemmer.Stem(word);
+                System.Console.WriteLine("{0} => {1}", word, stem);
+            }
+        }
+
+        private static void TimeTest()
+        {
+            var stemmer = new BulgarianStemmer();
+
+            var wordsRegex = new Regex(@"\w+", RegexOptions.Compiled);
+
+            const string text = @"Богдан Милчев, който оглавяваше КАТ-София, е преместен в резервния щат на МВР. Това означава, че той не е уволнен като служител, просто занапред няма да оглавява службата. Информацията беше потвърдена от вътрешния министър Цветлин Йовчев след церемонията за „Полицай на годината”.
+Йовчев мотивира решението си с липса на добри резултати от работата на Милчев и начина, по който ръководи КАТ-София.
+За временно изпълняващ длъжността директор е назначен заместникът на Милчев Иво Писанов. Пред dnevnik.bg Милчев заяви, че няма да коментира решението на Йовчев без разрешение на пресцентъра на МВР. 
+Милчев беше назначен за шеф на столичния КАТ през май 2011 г., след като МВР пенсионира цялото ръководство на звеното. Милчев обеща да е безкомпромисен към подкупите и заяви, че ще има коренна промяна в поведението на пътните полицаи.";
+
+            System.Console.WriteLine(text);
+
+            System.Console.WriteLine();
+            System.Console.WriteLine("=====");
+            System.Console.WriteLine();
+
+            Stopwatch tokenStopwatch = Stopwatch.StartNew();
+            var words = wordsRegex.Matches(text).OfType<Match>().Where(m => m.Success).Select(m => m.Value).ToArray();
+            tokenStopwatch.Stop();
+
+            Stopwatch stemStopwatch = Stopwatch.StartNew();
+            var stems = words.Select(stemmer.Stem).ToArray();
+            stemStopwatch.Stop();
+
+            foreach (string word in words)
+            {
+                System.Console.Write(word);
+                System.Console.Write(" ");
+            }
+
+            System.Console.WriteLine();
+            System.Console.WriteLine("=====");
+            System.Console.WriteLine();
+
+            foreach (string stem in stems)
+            {
+                System.Console.Write(stem);
+                System.Console.Write(" ");
+            }
+
+            System.Console.WriteLine();
+            System.Console.WriteLine("=====");
+            System.Console.WriteLine();
+
+
+            System.Console.WriteLine("Tokenization: {0}", tokenStopwatch.Elapsed);
+            System.Console.WriteLine("Stemming: {0}", stemStopwatch.Elapsed);
+        }
+    }
+}
---
+++ BulgarianStemmer/BulgarianStemmer.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Reflection;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace BulgarianStemmer
+{
+    public class BulgarianStemmer
+    {
+        private const int MinRuleFreq = 2;
+
+        private static readonly CultureInfo BulgarianCultureInfo = CultureInfo.GetCultureInfo("bg-BG");
+
+        private static readonly Regex BulgarianVowel = new Regex(@"[аъоуеияю]", RegexOptions.Compiled);
+        private static readonly Regex Pattern = new Regex(@"(\w+)\s==>\s(\w+)\s(\d+)", RegexOptions.Compiled);
+
+        private readonly Dictionary<string, string> _stemmingRules = new Dictionary<string, string>();
+
+        public BulgarianStemmer()
+        {
+            LoadEmbeddedRules();
+        }
+
+        private void LoadEmbeddedRules(int minRuleFreq = MinRuleFreq)
+        {
+            Assembly assembly = Assembly.GetAssembly(typeof(BulgarianStemmer));
+            Encoding encoding = Encoding.GetEncoding(1251);
+
+            Stream stemRulesContext1Stream = assembly.GetManifestResourceStream(@"BulgarianStemmer.Rules.stem_rules_context_1.txt");
+            Stream stemRulesContext2Stream = assembly.GetManifestResourceStream(@"BulgarianStemmer.Rules.stem_rules_context_2.txt");
+            Stream stemRulesContext3Stream = assembly.GetManifestResourceStream(@"BulgarianStemmer.Rules.stem_rules_context_3.txt");
+
+            if (stemRulesContext1Stream != null)
+                using (var reader = new StreamReader(stemRulesContext1Stream, encoding))
+                    LoadStemmingRules(reader, minRuleFreq);
+
+            if (stemRulesContext2Stream != null)
+                using (var reader = new StreamReader(stemRulesContext2Stream, encoding))
+                    LoadStemmingRules(reader, minRuleFreq);
+
+            if (stemRulesContext3Stream != null)
+                using (var reader = new StreamReader(stemRulesContext3Stream, encoding))
+                    LoadStemmingRules(reader, minRuleFreq);
+        }
+
+        public void LoadStemmingRules(TextReader reader, int minRuleFreq = MinRuleFreq)
+        {
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                if (String.IsNullOrWhiteSpace(line))
+                    continue;
+
+                Match m = Pattern.Match(line);
+                if (m.Success && Int32.Parse(m.Groups[3].Value) > MinRuleFreq)
+                {
+                    _stemmingRules[m.Groups[1].Value] = m.Groups[2].Value;
+                }
+            }
+        }
+
+        public void ClearStemmingRules()
+        {
+            _stemmingRules.Clear();
+        }
+
+        public string Stem(String word)
+        {
+            //Convert to lower case
+            string lowerWord = word.ToLower(BulgarianCultureInfo);
+
+            Match m = BulgarianVowel.Match(lowerWord);
+            if (!m.Success) // No Bulgarian vowel -> not a stemmable word
+                return lowerWord;
+
+            for (int i = m.Index + m.Length + 1; i < lowerWord.Length; i++)
+            {
+                string suffix = lowerWord.Substring(i);
+                string value;
+                //Check for stem suffix
+                if (_stemmingRules.TryGetValue(suffix, out value))
+                {
+                    return lowerWord.Substring(0, i) + value;
+                }
+            }
+            return lowerWord;
+        }
+    }
+}
